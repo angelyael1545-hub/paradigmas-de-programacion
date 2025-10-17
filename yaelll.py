41d4ceddd4106538a9468b06762d55aabfeaa2e1
angelyael1545-hub <angelyael1545@gmail.com>
2025-10-16 18:24:28 -0600

    Add files via upload

---
+++ yaelll.py
@@ -0,0 +1,54 @@
+#Solicitar al usuario la cantidad de datos a analizar
+n = int(input("¿Cuántos datos deseas ingresar? "))
+
+# Leer los datos
+datos = []
+contadore
+while contador < n:
+valor = float(input(f"Ingrese el valor (contador + 1): "))
+datos.append(valor)
+contador
+Análisis con ciclo while
+indice e
+suna e
+maxino datos[0]
+minino datos[@]
+positivos - e
+negativos - 0
+ceros
+while indice <n:
+valor datos [indice]
+suma + valor
+if valor maximo:
+maximo valor
+if valor < minimo:
+minimo valor
+if valor > 0:
+positivos +1 :
+elif valor < 0
+negativos + 1
+else:
+ceros 1
+indice 1
+Resultados
+promedio suma / n
+GoProject Petences Help
+EVACIA
+if valor > 0:
+positivos + 1
+elif valor < 8:
+negativos += 1
+else:
+ceros + 1
+indice + 1
+#Resultados
+promedio suma / n
+print("\nResultados del analisis:")
+print("Datos ingresados:", datos)
+prnt("Suma:", suma)
+print("Promedio:", promedio)
+print("Valor máximo:", maximo)
+print("Valor minimo:", minimo)
+print("Cantidad de positivos:", positivos)
+print("Cantidad de negativos:", negativos)
+print("Cantidad de ceros:", ceros)
