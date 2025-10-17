37b20931ebd7bf4915ecf8e4ab6386dfdeb545ee
angelyael1545-hub <angelyael1545@gmail.com>
2025-10-16 18:24:13 -0600

    Add files via upload

---
+++ yaell.py
@@ -0,0 +1,35 @@
+Lista de calificaciones de estudiantes
+calificaciones [85, 68, 78, 45, 92, 55, 70, 100, 39, 68, 80, 59]
+1. Clasificar cada calificación usando if else
+print("Clasificación de cada calificación:")
+aprobados = 0
+reprobados = 0
+for cal in calificaciones: for cal in calificaciones:
+for cal in calificaciones:
+if cal 60:
+print(f"(cal): Aprobado")
+aprobados + 1
+aise:
+print(f"(cal): Reprobado")
+reprobados + 1
+2. Contar cuántos aprobaron y reprobaron
+print("\nTotal de aprobados:", aprobados)
+print("Total de reprobados:", reprobados)
+3. Encontrar la calificación más alta y más baja usando if else
+naxina calificaciones[0]
+minina calificaciones[0]
+for cal in calificaciones:
+if cal maxina:
+maxima cal
+I
+if cal < minima:
+    minima cal
+print("\nLa calificación más alta es:", maxima)
+print("La calificación más baja es:", minima)
+#4. Analizar si cada calificación es par o impar usando if else
+print("\nParidad de cada calificación:")
+for cal in calificaciones:
+if cal % 2 0:
+print(f"(cal) es par")
+else:
+print(f"(cal) es impar")
